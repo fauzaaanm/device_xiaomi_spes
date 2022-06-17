b3a8edd091eaa8f5ca2f586e7e8b685001962760
buglessx <fauzan@nusantararom.org>
2022-06-17 17:12:57 +0000

    spes: Project Elixir Bringup
    Signed-off-by: buglessx <fauzan@nusantararom.org>

--- aosp_spes.mk
+++ aosp_spes.mk
@@ -11,16 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Inherit some common Nusantara stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
-NAD_BUILD_TYPE := OFFICIAL
+# Inherit some common Project Elixir stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
 TARGET_USES_BLUR := true
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_FACE_UNLOCK_SUPPORTED := true
 USE_PIXEL_CHARGING := true
 
 # Product Specifics
-PRODUCT_NAME := nad_spes
+PRODUCT_NAME := aosp_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
@@ -31,3 +30,12 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 # GMS
 TARGET_GAPPS_ARCH := arm64
 USE_GAPPS := true
+
+# Elixir Stuff
+TARGET_SUPPORTS_QUICK_TAP := true
+CUSTOM_BUILD_TYPE := OFFICIAL
+IS_PHONE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ACORE := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
